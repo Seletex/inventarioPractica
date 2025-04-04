80f25c12b591a9d188fbfad262adec95f0637fc1
Steven Ruiz <sarr1998@hotmail.com>
2025-04-04 13:33:12 -0500

    Creación de proyecto

---
+++ inventarioFront/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Inventario</h1>
+      <p>Bienvenido al sistema de inventario</p>
+    </div>
+  )
+ 
+}
+
+export default App
